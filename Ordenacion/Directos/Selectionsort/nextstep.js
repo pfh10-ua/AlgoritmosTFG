e9d614c8d342cb1859f12d067ed031bc725a10c4
Pedro F. <pfh10@alu.ua.es>
2025-03-11 16:15:52 +0100

    Mejorando legibilidad

--- Ordenacion/Directos/Selectionsort/nextstep.js
+++ Ordenacion/Directos/Selectionsort/nextstep.js
@@ -1,8 +1,10 @@
 module.exports ={
     variables: {
-        i: 0, // Índice de la iteración externa
-        j: 1, // Índice de la iteración interna
-        minIndex: 0, // Índice del mínimo encontrado
+        indices:{
+            i:0,
+            j:1,
+            minIndex:0
+        },
         colors: ["orange", "yellow", "red"], // Colores para cada barra
         sorted: false, // Indica si el arreglo ya está ordenado
         data: [30, 80, 45, 60, 20, 90, 50] // Datos a ordenar
@@ -12,28 +14,29 @@ module.exports ={
 
         if (variables.sorted) return; // Si ya está ordenado, no hacer nada
 
-        if (variables.i >= variables.data.length - 1) {
+        const {indices, data } = variables;
+
+        if (indices.i >= data.length - 1) {
             variables.sorted = true; // Marcar como ordenado
             renderGraphic(variables); // Actualizar el gráfico
             return;
         }
 
         // Comparar y actualizar el índice del mínimo
-        if (variables.data[variables.j] < variables.data[variables.minIndex]) {
-            variables.minIndex = variables.j; // Actualizar el índice del mínimo
+        if (data[indices.j] < data[indices.minIndex]) {
+            indices.minIndex = indices.j; // Actualizar el índice del mínimo
         }
 
-        variables.j++; // Avanzar al siguiente índice de la iteración interna
+        indices.j++; // Avanzar al siguiente índice de la iteración interna
 
-        if (variables.j === variables.data.length) {
+        if (indices.j === data.length) {
             // Intercambiar los elementos data[i] y data[minIndex]
-            [variables.data[variables.i], variables.data[variables.minIndex]] =
-                [variables.data[variables.minIndex], variables.data[variables.i]];
+            [data[indices.i], data[indices.minIndex]] = [data[indices.minIndex], data[indices.i]];
 
             // Avanzar a la siguiente iteración externa
-            variables.i++;
-            variables.j = variables.i + 1;
-            variables.minIndex = variables.i;
+            indices.i++;
+            indices.j = indices.i + 1;
+            indices.minIndex = indices.i;
         }
 
         renderGraphic(variables); // Actualizar el gráfico
