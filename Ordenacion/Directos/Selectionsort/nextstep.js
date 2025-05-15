da6961e1de95eb8ddb6065b30bc93010791411d8
Pedro <pfh10@alu.ua.es>
2025-05-15 19:53:58 +0200

    Cambiando nombre a sorted por sortedOrFind

--- Ordenacion/Directos/Selectionsort/nextstep.js
+++ Ordenacion/Directos/Selectionsort/nextstep.js
@@ -6,7 +6,7 @@ module.exports ={
             minIndex:0
         },
         colors: ["orange", "yellow", "red"], // Colores para cada barra
-        sorted: false, // Indica si el arreglo ya está ordenado
+        sortedOrFind: false, // Indica si el arreglo ya está ordenado
         data: [30, 80, 45, 60, 20, 90, 50], // Datos a ordenar
         lineaActual: {
             cpp: 8, 
@@ -18,12 +18,12 @@ module.exports ={
     nextstep: function(){
         const { variables } = this; // Acceder a las variables del objeto exportado
 
-        if (variables.sorted) return; // Si ya está ordenado, no hacer nada
+        if (variables.sortedOrFind) return; // Si ya está ordenado, no hacer nada
 
         const {indices, data, language } = variables;
 
         if (indices.i >= data.length - 1) {
-            variables.sorted = true; // Marcar como ordenado
+            variables.sortedOrFind = true; // Marcar como ordenado
             variables.lineaActual[language] = this.getFinalLine(language); //Línea final
             return;
         }
@@ -53,32 +53,28 @@ module.exports ={
     getFinalLine: function (language) {
         switch (language) {
             case 'cpp': return 15;
-            // case 'py': return 14;
-            // case 'java': return 16;
+            case 'py': return 17;
             default: return '-';
         }
     },
     getUpdateMinIndexLine: function (language) {
         switch (language) {
             case 'cpp': return [9,10];
-            // case 'py': return 8;
-            // case 'java': return 10;
+            case 'py': return [13,14];
             default: return '-';
         }
     },
     getDefaultComparisonLine: function (language) {
         switch (language) {
             case 'cpp': return 8;
-            // case 'py': return 7;
-            // case 'java': return 9;
+            case 'py': return 12;
             default: return '-';
         }
     },
     getSwapLine: function (language) {
         switch (language) {
             case 'cpp': return 13;
-            // case 'py': return 12;
-            // case 'java': return 14;
+            case 'py': return 16;
             default: return '-';
         }
     }
